5d88d7074fefb50b6658143bd140078d970e8d2e
dondaqb <a21vlad2@gmail.com>
2026-05-08 12:15:52 +0300

    Сделал правильное меню

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// function openImageModal(imageSrc) {
-//     const modal = document.getElementById('image-modal');
-//     const modalImg = document.getElementById('modal-image');
-    
-//     modal.style.display = "block";
-//     modalImg.src = imageSrc; // Подставляем путь к картинке меню
-// }
 
-// function closeImageModal() {
-//     document.getElementById('image-modal').style.display = "none";
-// }
-
-// // Закрытие при клике вне окна
-// window.onclick = function(event) {
-//     const modal = document.getElementById('image-modal');
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
-// Обнови эти функции в script.js
 function openImageModal(imageSrc) {
     const modal = document.getElementById('image-modal');
     const modalImg = document.getElementById('modal-image');
